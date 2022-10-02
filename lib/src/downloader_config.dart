e08655d3ea5174e1074bee81ecc5d701d4228a9a
Tyler Nijmeh <tylernij@gmail.com>
2022-10-02 14:25:36 -0700

    Add download mode and optimize config
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/downloader_config.dart
+++ lib/src/downloader_config.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:meow_dart/src/format.dart';
+import 'package:meow_dart/src/mode.dart';
 
 /// The configuration for the download.
 class DownloaderConfig {
@@ -8,8 +9,9 @@ class DownloaderConfig {
   DownloaderConfig({
     required this.videoId,
     required this.directory,
-    required this.format,
-    required this.command,
+    this.format = Format.muxed,
+    this.mode = Mode.video,
+    this.command,
   });
 
   /// The YouTube video ID to use.
@@ -20,6 +22,9 @@ class DownloaderConfig {
 
   /// The download format type.
   final Format format;
+
+  /// The download mode.
+  final Mode mode;
 
   /// A command to run after each download has been completed.
   final String? command;
---
+++ lib/src/mode.dart
@@ -0,0 +1,8 @@
+/// The download mode.
+enum Mode {
+  /// The URL represents a video.
+  video,
+
+  /// The URL represents a playlist.
+  playlist,
+}
